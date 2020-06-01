e252c1e774a281073286bb78f8e655aad32e40ef
GINON Georges <georges.ginon@gmail.com>
2020-06-01 18:39:25 +0200

    Understanding State Data Changes

--- src/js/App.js
+++ src/js/App.js
@@ -2,23 +2,30 @@ import React, { Component } from 'react';
 
 class App extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
         this.state = {
             username: "Georges"
         }
     }
 
-    render() {
-        return (
-            <div>
-                <h4 className="bg-primary text-white text-center p-2">
-                    { this.state.username }'s Todo List
-                </h4>
-            </div>
-        );
+    changeStateData = () => {
+        console.log('=> changeStateData');
+        this.setState({
+            username: this.state.username === "Georges" ? "Marion" : "Georges"
+        })
     }
-    
+
+    render = () =>
+        <div>
+            <h4 className="bg-primary text-white text-center p-2">
+                {this.state.username}'s Todo List
+            </h4>
+            <button onClick={this.changeStateData}>
+                Change username
+            </button>
+        </div>
+
 }
 
 export default App;

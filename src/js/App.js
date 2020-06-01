7c00df03d1e04ea85890218dbb814edd1f0844d7
GINON Georges <georges.ginon@gmail.com>
2020-06-01 17:41:41 +0200

    displaying dynamic content

--- src/js/App.js
+++ src/js/App.js
@@ -1,10 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-class App extends React.Component {
+class App extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {
+            username: "Georges"
+        }
+    }
+
     render() {
         return (
             <div>
-                <h4 className="bg-primary text-white text-center p-2">To Do List</h4>
+                <h4 className="bg-primary text-white text-center p-2">
+                    { this.state.username }'s Todo List
+                </h4>
             </div>
         );
     }
